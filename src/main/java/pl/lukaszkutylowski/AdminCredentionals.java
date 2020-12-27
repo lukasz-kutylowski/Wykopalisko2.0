23a50d56ace1db031d65730a1d66d1187255bc82
lukasz-kutylowski <76553842+lukasz-kutylowski@users.noreply.github.com>
2020-12-27 19:24:25 +0100

    Create Admin Credentials

---
+++ src/main/java/pl/lukaszkutylowski/AdminCredentionals.java
@@ -0,0 +1,6 @@
+package pl.lukaszkutylowski;
+
+public class AdminCredentionals {
+    public static final String ADMIN_USERNAME = "[username - deleted by developer]";
+    public static final String ADMIN_PASSWORD = "[password - deleted by developer]";
+}
